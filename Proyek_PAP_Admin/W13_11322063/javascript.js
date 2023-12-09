1884c6bc9d84a368727ee591798829485ac423b3
aqustin <124247570+aqustin@users.noreply.github.com>
2023-12-09 22:32:31 +0700

    Add files via upload

---
+++ Proyek_PAP_Admin/W13_11322063/javascript.js
@@ -0,0 +1,5 @@
+document.getElementById("searchForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Add your search functionality here
+    alert("Search functionality to be implemented");
+});
